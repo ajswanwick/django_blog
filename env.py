823bf9fab50b220349675504ee824a930514e0ef
Adam Swanwick <a.j.swanwick@gmail.com>
2024-12-10 12:07:01 +0000

    update databases

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:8DwEak3viCHT@ep-nameless-lake-a2pagrka.eu-central-1.aws.neon.tech/skate_kitty_dawn_125042")
+    "DATABASE_URL", "postgresql://neondb_owner:8DwEak3viCHT@ep-nameless-lake-a2pagrka.eu-central-1.aws.neon.tech/spool_vocal_reply_894521")
 
 os.environ.setdefault(
  "SECRET_KEY", "VEQZiFrEVW#^o*88"
