da46ad5e202f85b5328738082057bdecc1078e3a
Adam Swanwick <a.j.swanwick@gmail.com>
2024-12-16 11:44:17 +0000

    enable of serving image files

--- env.py
+++ env.py
@@ -6,3 +6,6 @@ os.environ.setdefault(
 os.environ.setdefault(
  "SECRET_KEY", "VEQZiFrEVW#^o*88"
 )
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@dsg0ziqng")
